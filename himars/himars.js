0383b6f3a7166afa63fbdf3dcda15bdba51dc216
Pierre-Évariste Dagand <pierre@evr.ist>
2025-11-19 08:45:25 +0100

    Himars: first working version

--- himars/himars.js
+++ himars/himars.js
@@ -1,28 +1,228 @@
-/* function setup() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+"use strict";
 
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 150, 100);
-} */
+const debug = true
 
-function pixmap(m) {
-  let px_hz = 10
-  let px_vt = 10
-  let draw = SVG()
+const width = 128
+const height = 72
 
-  for (let i = 0; i < m.length; i++) {
-    for (let j = 0; j < m.length; j++) {
-      draw.rect(px_hz, px_vt).move(i * px_hz, j * px_vt).fill(m[i][j])
+
+class Pixel {
+  constructor(x, y, color) {
+    this.x = x
+    this.y = y
+    this.color = color
+  }
+
+  static scale(x) {
+    return x * 8
+  }
+
+  draw(o) {
+    o.rect(Pixel.scale(1), Pixel.scale(1)).move(Pixel.scale(this.x), Pixel.scale(this.y)).fill(this.color)
+  }
+
+}
+
+class Pixmap {
+  constructor(m) {
+    this.pixmap = m
+  }
+
+  svg() {
+    const draw = SVG()
+
+    for (let i = 0; i < this.pixmap.length; i++) {
+      for (let j = 0; j < this.pixmap[i].length; j++) {
+        if (this.pixmap[i][j] != '') {
+          let px = new Pixel(j, i, this.pixmap[i][j])
+          px.draw(draw)
+        }
+      }
+    }
+
+    const img = new Image()
+    img.src = "data:image/svg+xml;base64," + btoa(draw.svg())
+
+    return img
+  }
+
+  flip() {
+    return new Pixmap(this.pixmap.map(element => element.reverse()))
+  }
+
+  draw(ctx, x, y) {
+    ctx.drawImage(this.svg(), Pixel.scale(x), Pixel.scale(height - this.pixmap.length - y));
+  }
+}
+
+function plane(color) {
+  return [
+    [color, '', '', ''],
+    [color, color, '', ''],
+    [color, color, color, color],
+    [color, color, '', ''],
+    [color, '', '', '']
+  ]
+}
+
+function himars(color) {
+  return [
+    ['', '', '', color, '', ''],
+    ['', '', color, '', '', ''],
+    ['', color, '', '', color, color],
+    [color, '', '', '', color, color],
+    [color, 'Brown', color, color, 'Brown', color]]
+}
+
+const sprite_plane_friend = new Pixmap(plane('Blue'))
+const sprite_plane_foe = (new Pixmap(plane('Red'))).flip()
+const sprite_himars_friend = new Pixmap(himars('Blue'))
+const sprite_himars_foe = (new Pixmap(himars('Red'))).flip()
+const sprite_missile = new Pixmap([['Orange']])
+
+const canvas = document.getElementById("canvas");
+canvas.width = Pixel.scale(width)
+canvas.height = Pixel.scale(height)
+const ctx = canvas.getContext("2d");
+
+function background(ctx) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = 'Brown'
+  ctx.fillRect(0, Pixel.scale(height - 1), Pixel.scale(width), Pixel.scale(1))
+  ctx.fillStyle = 'Blue'
+  ctx.fillRect(Pixel.scale(7 * width / 16), Pixel.scale(height - 1), Pixel.scale(width / 8), Pixel.scale(1))
+}
+
+
+var pos_himars_friend = width / 4
+var pos_himars_foe = 3 * width / 4
+
+var missiles = []
+var planes = []
+
+function draw(ctx) {
+  background(ctx)
+
+  sprite_himars_friend.draw(ctx, pos_himars_friend, 1)
+  sprite_himars_foe.draw(ctx, pos_himars_foe, 1)
+  missiles.forEach(m => sprite_missile.draw(ctx, m.x, m.y))
+  planes.forEach(m => m.sprite.draw(ctx, m.x, m.y))
+}
+
+const eventQueue = []
+
+function in_river(x_min, x_max) {
+  return x_max > 7 * width / 16 && x_min < 9 * width / 16
+}
+
+class MoveHimars {
+  constructor(himars, dir) {
+    this.himars = himars
+    this.dir = dir
+  }
+
+  doit() {
+    if (this.himars === "friend") {
+      pos_himars_friend += this.dir
+      if (in_river(pos_himars_friend, pos_himars_friend + 6) || pos_himars_friend < 0) {
+        pos_himars_friend -= this.dir
+      }
+    } else if (this.himars == "foe") {
+      pos_himars_foe += this.dir
+      if (in_river(pos_himars_foe, pos_himars_foe + 6) || pos_himars_foe + 6 > width) {
+        pos_himars_foe -= this.dir
+      }
+    } else {
+      assert(False)
+    }
+  }
+}
+
+function start_plane(sprite, pos_x, dx) {
+  planes.push({ sprite: sprite, x: pos_x, y: height / 2 + Math.floor(Math.random() * (height / 2)), dx })
+}
+
+function launch_missile(pos_x, dx, dy) {
+  missiles.push({ x: pos_x, y: 6, dx, dy })
+}
+
+class Fire {
+  constructor(himars) {
+    this.himars = himars
+  }
+
+  doit() {
+    if (this.himars === "friend") {
+      launch_missile(pos_himars_friend + 4, 1, 1)
+    } else if (this.himars === "foe") {
+      launch_missile(pos_himars_foe + 1, -1, 1)
+    } else {
+      assert(False)
+    }
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    eventQueue.push(new MoveHimars('foe', 1))
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    eventQueue.push(new MoveHimars('foe', -1))
+  } else if (e.key === "a") {
+    eventQueue.push(new MoveHimars('friend', -1))
+  } else if (e.key === "s") {
+    eventQueue.push(new MoveHimars('friend', 1))
+  } else if (e.key === "w") {
+    eventQueue.push(new Fire('friend'))
+  } else if (e.key === "Up" || e.key === "ArrowUp") {
+    eventQueue.push(new Fire('foe'))
+  } else {
+    console.log(`Ignored: ${e}`)
+  }
+}
+
+document.addEventListener("keyup", keyUpHandler);
+// document.addEventListener("keydown", keyDownHandler);
+
+function hits(plane_x, plane_y, missile_x, missile_y){
+  return plane_x <= missile_x && missile_x <= plane_x + 4 &&
+          plane_y <= missile_y && missile_y <= plane_y +5
+}
+
+function is_hit(x, y, missiles){
+  return missiles.some(m => hits(x, y, m.x, m.y))
+}
+
+var stepCount = 0
+
+function step() {
+  stepCount += 1
+
+  var ev
+  while (ev = eventQueue.pop()) {
+    ev.doit()
+  }
+  if (stepCount % 4 == 0 && Math.random() > 0.99) {
+    if (Math.random() > 0.5) {
+      start_plane(sprite_plane_friend, 0, 1)
+    } else {
+      start_plane(sprite_plane_foe, width, -1)
     }
   }
 
-  return draw
+  draw(ctx)
+
+  missiles.forEach(m => { m.x = m.x + m.dx; m.y = m.y + m.dy })
+  missiles = missiles.filter(m => m.y < height)
+
+  if (stepCount % 8 == 0) {
+    planes.forEach(m => { m.x = m.x + m.dx })
+  }
+  planes = planes.filter(p => ! is_hit(p.x, p.y, missiles))
+
+
+  requestAnimationFrame(step)
 }
 
-let draw = pixmap([['#f03', '#a03'],
-['#ff', '#98']])
-
-SVG.on(document, 'DOMContentLoaded', function () {
-  draw.addTo('body')
-})
+// Event loop
+requestAnimationFrame(step)
