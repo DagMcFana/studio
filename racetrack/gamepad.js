fd908dc7e52c57c8b89093313ea394df681e9739
Pierre-Évariste Dagand <pierre@evr.ist>
2026-03-19 17:45:59 +0100

    Enable joystick

--- racetrack/gamepad.js
+++ racetrack/gamepad.js
@@ -3,40 +3,50 @@
 const deadzone = 0.18;
 
 function getGamepads() {
-  return navigator.getGamepads ? navigator.getGamepads() : [];
+    return navigator.getGamepads ? navigator.getGamepads() : [];
 }
 
-// function applyDeadzone(value) {
-//  if (Math.abs(value) < deadzone) return 0;
-//  return value;
-// }
+function applyDeadzone(value) {
+    if (Math.abs(value) < deadzone) return 0;
+    return value;
+}
 
 function readGamepad(pad, prevButtons, action) {
-  let x = 0;
+    let x = 0;
+    let y = 0
+
+    if (!pad) { assert(false) }
+
+    const axisX = applyDeadzone(pad.axes[0] || 0);
+    const axisY = applyDeadzone(pad.axes[1] || 0);
+
+    x = axisX;
+    y = axisY;
+    const dpadLeft = pad.buttons[14] && pad.buttons[14].value == 1;
+    const dpadRight = pad.buttons[15] && pad.buttons[15].value == 1;
+
+    if (dpadLeft) x = 1;
+    if (dpadRight) x = -1;
+
+    if (x != 0) {
+        action.rotate(x);
+    }
 
 
-  if (!pad) { assert(false) }
+    const currentButtons = [
+        pad.buttons[0] && pad.buttons[0].pressed,
+        pad.buttons[1] && pad.buttons[1].pressed,
+    ];
 
-  const dpadLeft = pad.buttons[14] && pad.buttons[14].value == 1;
-  const dpadRight = pad.buttons[15] && pad.buttons[15].value == 1;
-
-  if (dpadLeft) x = 1;
-  if (dpadRight) x = -1;
-
-  if (x != 0) {
-    action.rotate(x);
-  }
+    if (currentButtons[0] && !prevButtons[0]) y = -1;
+    if (currentButtons[1] && !prevButtons[1]) y = 1;
 
 
-  const currentButtons = [
-    pad.buttons[0] && pad.buttons[0].pressed,
-    pad.buttons[1] && pad.buttons[1].pressed,
-  ];
+    prevButtons[0] = currentButtons[0];
+    prevButtons[1] = currentButtons[1]
 
-  if (currentButtons[0] && !prevButtons[0]) action.speed(-1);
-  if (currentButtons[1] && !prevButtons[1]) action.speed(1);
 
-  prevButtons[0] = currentButtons[0];
-  prevButtons[1] = currentButtons[1]
-
+    if (y != 0) {
+        action.speed(y);
+    }
 }
